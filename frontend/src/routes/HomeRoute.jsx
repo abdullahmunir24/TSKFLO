f5056e784031d0487617f1320fa6d206585d7f36
Abdullah Munir <abdullahmunir2004@gmail.com>
2025-02-05 23:32:33 -0800

    Chnaged navbar

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage'; 
+import HomePage from '../pages/HomePage';
 import UserDashboard from '../pages/UserDashboard';
-import TaskManagement from '../pages/TaskManagement'; 
+import TaskManagement from '../pages/TaskManagement';
 
 const HomeRoute = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        
         <Route path="/dashboard" element={<UserDashboard />} />
-        
         <Route path="/tasks" element={<TaskManagement />} />
       </Routes>
     </Router>
